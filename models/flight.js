f431eb87a47c22877ff9f5c640832aa90d903a99
Andrew R <andrew.rentschler@gmail.com>
2023-04-20 09:09:26 -0500

    Fix Import

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number,
+    min: 0
+  },
+  
+}, {
+  timestamps: true
+})
+
 const flightSchema = new Schema({
   airline: {
     type: String,
@@ -26,19 +40,7 @@ const flightSchema = new Schema({
   timestamps: true
 })
 
-const ticketSchema = new Schema({
-  seat: {
-    type: String,
-    match: /[A-F][1-9]\d?/
-  },
-  price: {
-    type: Number,
-    min: 0
-  },
-  
-}, {
-  timestamps: true
-})
+
 
 
 
